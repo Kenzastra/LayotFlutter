36123160fe8745e91e10a1a6704c092452f465df
Kenzastra <100890561+Kenzastra@users.noreply.github.com>
2023-08-04 14:13:54 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'main_menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Layout',
+      home: MainMenu()
+    );
+  }
+}
+
+
+
+
+
+
